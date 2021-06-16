90b8677e955ea632c6badcb64d23b883fa6d192c
Maurizio <mauriziodm@gmail.com>
2021-06-15 22:28:47 -0300

    created input validation text creation and and enhanced click event code

--- js/script.js
+++ js/script.js
@@ -2,9 +2,26 @@ var button = document.getElementById('enter');
 var input = document.getElementById('userinput');
 var ul = document.querySelector('ul');
 
+function inputValidation(){
+    return input.value.length;
+};
+
+function createText(){
+    var li = document.createElement('li');
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value= '';
+};
+
 button.addEventListener("click", function(){
+    if(inputValidation() > 0){
+        createText();
+    }
+});
+
+/*input.addEventListener("keypress", function(event){
     var li = document.createElement('li');
     li.appendChild(document.createTextNode("testing"));
     ul.appendChild(li);
-});
+});*/
 
